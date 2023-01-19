11d61b43fa25925d6e7b92d7402a388e614925f9
Ben Greanya <bengreanya@gmail.com>
2023-01-19 15:52:30 -0800

    more styling

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -2,17 +2,15 @@ import React from 'react';
 
 export default function Controls({ types, handler }) {
   return (
-    <div>
-      <select onChange={(e) => handler(e.target.value)}>
-        <option value="">All</option>
-        {types.map(({ type, count }) => {
-          return (
-            <option key={type} value={type}>
-              {type}-{count}
-            </option>
-          );
-        })}
-      </select>
-    </div>
+    <select onChange={(e) => handler(e.target.value)}>
+      <option value="">All</option>
+      {types.map(({ type, count }) => {
+        return (
+          <option key={type} value={type}>
+            {type}-{count}
+          </option>
+        );
+      })}
+    </select>
   );
 }
