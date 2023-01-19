84f5a53aaab5928c45acfa710037bfb0ea98ed5a
Ben Greanya <bengreanya@gmail.com>
2023-01-19 15:37:00 -0800

    added styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,18 +2,20 @@ import React from 'react';
 import { usePokemon } from '../hooks/pokemon.js';
 import PokeCard from './PokeCard.js';
 import Controls from './Controls.js';
-
+import './Main.css';
 export default function Main() {
   const { pokemons, types, handleFilterSelection, loading } = usePokemon();
   if (loading) {
     return <h1>Loading</h1>;
   } else {
     return (
-      <div>
+      <div className="main-div">
         <Controls types={types} handler={handleFilterSelection} />
-        {pokemons.map((pokemon) => {
-          return <PokeCard key={pokemon.id} pokemon={pokemon} />;
-        })}
+        <section className="card-container">
+          {pokemons.map((pokemon) => {
+            return <PokeCard key={pokemon.id} pokemon={pokemon} />;
+          })}
+        </section>
       </div>
     );
   }
