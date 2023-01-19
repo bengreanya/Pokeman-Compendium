2fb8a21ef3c25f65bbcbc37351bf4289500439e5
spiderforrest <spider@spood.org>
2023-01-19 15:10:51 -0800

    okay so loading works

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,13 +4,17 @@ import PokeCard from './PokeCard.js';
 import Controls from './Controls.js';
 
 export default function Main() {
-  const { pokemons, types, handleFilterSelection } = usePokemon();
-  return (
-    <div>
-      <Controls types={types} handler={handleFilterSelection} />
-      {pokemons.map((pokemon) => {
-        return <PokeCard key={pokemon.id} pokemon={pokemon} />;
-      })}
-    </div>
-  );
+  const { pokemons, types, handleFilterSelection, loading } = usePokemon();
+  if (loading) {
+    return <h1>Loading</h1>;
+  } else {
+    return (
+      <div>
+        <Controls types={types} handler={handleFilterSelection} />
+        {pokemons.map((pokemon) => {
+          return <PokeCard key={pokemon.id} pokemon={pokemon} />;
+        })}
+      </div>
+    );
+  }
 }
--- src/hooks/pokemon.js
+++ src/hooks/pokemon.js
@@ -4,25 +4,32 @@ import { getPokemon, getTypes } from '../services/getPokemon.js';
 export function usePokemon() {
   const [pokemons, setPokemons] = useState([]);
   const [types, setTypes] = useState([]);
+  const [loading, setLoading] = useState([]);
   // get pokemon
   useEffect(() => {
+    setLoading(true);
     const getData = async () => {
       const data = await getPokemon();
       setPokemons(data.results);
+      setLoading(false);
     };
     getData();
   }, []);
   // get types
   useEffect(() => {
+    setLoading(true);
     const getData = async () => {
       const data = await getTypes();
       setTypes(data);
+      setLoading(false);
     };
     getData();
   }, []);
   const handleFilterSelection = async (type) => {
+    setLoading(true);
     const data = await getPokemon(type);
     setPokemons(data.results);
+    setLoading(false);
   };
-  return { pokemons, types, handleFilterSelection };
+  return { pokemons, types, handleFilterSelection, loading };
 }
